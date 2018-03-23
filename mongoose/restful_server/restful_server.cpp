2e6145a9d13a9b920f2cb6fd7e41cda488e1e52e
StrikeW <wangsiyuanse@gmail.com>
2018-03-23 22:43:24 +0800

    move to c++

--- mongoose/restful_server/restful_server.cpp
+++ mongoose/restful_server/restful_server.cpp
@@ -8,9 +8,14 @@
 #include <zmq.h>
 #include <pthread.h>
 #include <assert.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdint.h>
+#include <errno.h>
+#include <list>
 
 typedef void* thread_func_t (void *);
+#define REDIS_COMMAND (redisReply *)redisCommand
 
 static const char *s_http_port = "8000";
 static struct mg_serve_http_opts s_http_server_opts;
@@ -18,28 +23,46 @@ static struct mg_serve_http_opts s_http_server_opts;
 static redisContext *redis_cli;
 static void *zmq_ctx;
 
-#define KEY_LEN 32
+#define MAX_KEY_LEN 32
+#define MAX_VALUE_LEN 2048
+
+static char key_[MAX_KEY_LEN];
+static char value_[MAX_VALUE_LEN];
+
+int global_num_msgs = 1;
 
 
-typedef struct {
-    uint32_t msg_len;    // aligened to 8 bytes
-
-    struct msg_body {
+struct msg_body {
         uint32_t value_len;
-        char key[KEY_LEN];
-        char *value;
-    } body;
-} __attribute__((packed)) replicate_msg_t;
+        char key[MAX_KEY_LEN];
+        char value[MAX_VALUE_LEN];
+} __attribute__((packed));
+
+struct rep_msg_t {
+    // uint32_t msg_len;    // aligened to 8 bytes
+    // msg_body body;
+    uint32_t key_len;
+    uint32_t value_len;
+    char key[MAX_KEY_LEN];
+    char value[MAX_VALUE_LEN];
+} __attribute__((packed));
 
 
-typedef struct {
+struct replicater_t {
     pthread_t tid;
     void *cli_sock;
     void *srv_sock;
     thread_func_t* process;
     char remote_host[32];
     int remote_port;
-} replicater_t;
+    int local_port;
+    zmq_pollitem_t poll_items[1];
+};
+
+
+pthread_mutex_t mutex;
+std::list<rep_msg_t> msg_list;
+
 
 void redis_init()
 {
@@ -63,58 +86,71 @@ void *replicater_thread(void *arg)
     replicater_t *r = (replicater_t *)arg;
     assert(r->cli_sock != NULL);
     assert(r->srv_sock != NULL);
-    uint32_t msg_len, data_len;
-    struct msg_body *msg;
+    // uint32_t msg_len, data_len;
+    // struct msg_body *msg;
     char *buf;
 
     int rc;
+
+    char remote_addr[48];
+    char local_addr[48];
+
+    sprintf(remote_addr, "tcp://%s:%d", r->remote_host, r->remote_port);
+    sprintf(local_addr, "tcp://%s:%d", "127.0.0.1", r->local_port);
+    printf("DEBUG: remote_addr: %s\n", remote_addr);
+    printf("DEBUG: local_addr: %s\n", local_addr);
+
+    rc = zmq_bind(r->srv_sock, local_addr);
+    if (rc != 0) {
+        perror("zmq_bind");
+        assert(false);
+    }
+
+    rc = zmq_connect(r->cli_sock, remote_addr);
+    assert(rc == 0);
+    printf("DEBUG: sockets setup success\n");
+
     while (1) {
-        msg_len = 0;
-        rc = zmq_recv(r->srv_sock, &msg_len, sizeof(msg_len), 0);
-        if (rc == -1) {
-            perror("replicater: zmq_recv msg_len");
-            exit(1);
+        zmq_poll(r->poll_items, 1, 100);
+
+        if (r->poll_items[0].revents & ZMQ_POLLIN) {
+            rep_msg_t rep_msg;
+
+            rc = zmq_recv(r->srv_sock, &rep_msg, sizeof(rep_msg_t), 0);
+            if (rc == -1) {
+                perror("replicater: zmq_recv msg_len");
+                exit(1);
+            }
+            printf("DEBUG: recved message[key_len:%d, value_len:%d, key: %s, value:%s]\n",
+                    rep_msg.key_len, rep_msg.value_len, rep_msg.key, rep_msg.value);
         }
-        data_len = msg_len - sizeof(msg_len);
 
-        buf = (char*) malloc(data_len);
-        rc = zmq_recv(r->srv_sock, buf, sizeof(struct msg_body), 0);
-        if (rc == -1) {
-            perror("replicater: zmq_recv payload");
-            exit(1);
+        pthread_mutex_lock(&mutex);
+        while (!msg_list.empty()) {
+            rep_msg_t rep_msg = msg_list.front();
+            rc = zmq_send(r->cli_sock, &rep_msg, sizeof(rep_msg_t), 0);
+            if (rc == -1) {
+                perror("replicater: zmq_send msg");
+                exit(1);
+            }
+            msg_list.pop_front();
         }
-        msg = (struct msg_body *)buf;
-        assert(data_len == msg->value_len + KEY_LEN);
-
-
-        printf("replicater: received message[msg_len:%d, value_len:%d, value:%s]\n",
-                msg_len, msg->value_len, msg->value);
-
-
-        free(buf);
+        pthread_mutex_unlock(&mutex);
     }
 
 }
 
-void replicater_init(replicater_t *r)
+void replicater_init(replicater_t *r, int remote_port, int local_port)
 {
     zmq_ctx = zmq_ctx_new();
     r->cli_sock = zmq_socket(zmq_ctx, ZMQ_REQ);
     r->srv_sock = zmq_socket(zmq_ctx, ZMQ_REP);
     r->process = replicater_thread;
-    r->remote_port = 5555;
+    r->remote_port = remote_port;
+    r->local_port = local_port;
+    r->poll_items[0].socket = r->srv_sock;
+    r->poll_items[0].events = ZMQ_POLLIN;
 
-    char remote_addr[48];
-    int rc;
-    sprintf(remote_addr, "tcp:://%s:%d", r->remote_host, r->remote_port);
-    // client sock
-    printf("DEBUG: remote_addr: %s\n", remote_addr);
-
-    rc = zmq_connect(r->cli_sock, remote_addr);
-    assert(rc == 0);
-    rc = zmq_bind(r->srv_sock, "tcp://127.0.0.1:5555");
-    assert(rc == 0);
-    printf("DEBUG: replicater_init success\n");
 }
 
 static void handle_sum_call(struct mg_connection *nc, struct http_message *hm) {
@@ -132,7 +168,7 @@ static void handle_sum_call(struct mg_connection *nc, struct http_message *hm) {
   /* Compute the result and send it back as a JSON object */
   result = strtod(n1, NULL) + strtod(n2, NULL);
 
-  reply = redisCommand(redis_cli,"SET %s %d", "sum", (int)result);
+  reply = REDIS_COMMAND(redis_cli,"SET %s %d", "sum", (int)result);
   printf("REDIS_CLI: SET: %s\n", reply->str);
   freeReplyObject(reply);
 
@@ -173,13 +209,17 @@ int main(int argc, char *argv[]) {
 #endif
   redisReply *reply;
   replicater_t replicater;
+  int remote_port, local_port;
+
+
+  memset(&replicater, 0, sizeof(replicater));
 
   mg_mgr_init(&mgr, NULL);
   redis_init();
 
 
   /* PING server */
-  reply = redisCommand(redis_cli,"PING");
+  reply = REDIS_COMMAND(redis_cli,"PING");
   printf("REDIS_CLI: PING: %s\n", reply->str);
   freeReplyObject(reply);
 
@@ -194,29 +234,22 @@ int main(int argc, char *argv[]) {
     if (strcmp(argv[i], "-D") == 0 && i + 1 < argc) {
       mgr.hexdump_file = argv[++i];
     } else if (strcmp(argv[i], "--remote_host") == 0 && i + 1 < argc) {
-        strcmp(replicater.remote_host, argv[++i]);
-    // } else if (strcmp(argv[i], "--remote_port") == 0 && i + 1 < argc) {
-        // replicater.remote_port = atoi(argv[++i]);
+        strcpy(replicater.remote_host, argv[++i]);
+    } else if (strcmp(argv[i], "--remote_port") == 0 && i + 1 < argc) {
+        remote_port = atoi(argv[++i]);
+    } else if (strcmp(argv[i], "--local_port") == 0 && i + 1 < argc) {
+        local_port = atoi(argv[++i]);
+
+    } else if (strcmp(argv[i], "--key") == 0 && i + 1 < argc) {
+        strncpy(key_, argv[++i], MAX_KEY_LEN);
+
+    } else if (strcmp(argv[i], "--value") == 0 && i + 1 < argc) {
+        strncpy(value_, argv[++i], 1024);
+
     } else if (strcmp(argv[i], "-d") == 0 && i + 1 < argc) {
       s_http_server_opts.document_root = argv[++i];
     } else if (strcmp(argv[i], "-p") == 0 && i + 1 < argc) {
       s_http_port = argv[++i];
-    } else if (strcmp(argv[i], "-a") == 0 && i + 1 < argc) {
-      s_http_server_opts.auth_domain = argv[++i];
-    } else if (strcmp(argv[i], "-P") == 0 && i + 1 < argc) {
-      s_http_server_opts.global_auth_file = argv[++i];
-    } else if (strcmp(argv[i], "-A") == 0 && i + 1 < argc) {
-      s_http_server_opts.per_directory_auth_file = argv[++i];
-    } else if (strcmp(argv[i], "-r") == 0 && i + 1 < argc) {
-      s_http_server_opts.url_rewrites = argv[++i];
-#if MG_ENABLE_HTTP_CGI
-    } else if (strcmp(argv[i], "-i") == 0 && i + 1 < argc) {
-      s_http_server_opts.cgi_interpreter = argv[++i];
-#endif
-#if MG_ENABLE_SSL
-    } else if (strcmp(argv[i], "-s") == 0 && i + 1 < argc) {
-      ssl_cert = argv[++i];
-#endif
     } else {
       fprintf(stderr, "Unknown option: [%s]\n", argv[i]);
       exit(1);
@@ -224,20 +257,25 @@ int main(int argc, char *argv[]) {
   }
 
 
-  printf("calling replicater_init...\n");
-  replicater_init(&replicater);
+  replicater_init(&replicater, remote_port, local_port);
+  assert(replicater.remote_port > 0);
+  assert(replicater.local_port > 0);
   pthread_create(&(replicater.tid), NULL, replicater.process, &replicater);
   printf("replicater created!\n");
 
 
+  rep_msg_t rep_msg;
+  strncpy(rep_msg.key, key_, MAX_KEY_LEN);
+  rep_msg.key_len = strlen(rep_msg.key) + 1;
+  strncpy(rep_msg.value, value_, MAX_VALUE_LEN);
+  rep_msg.value_len = strlen(rep_msg.value) + 1;
+
+  msg_list.push_back(rep_msg);
+
   /* Set HTTP server options */
   memset(&bind_opts, 0, sizeof(bind_opts));
   bind_opts.error_string = &err_str;
-#if MG_ENABLE_SSL
-  if (ssl_cert != NULL) {
-    bind_opts.ssl_cert = ssl_cert;
-  }
-#endif
+
   nc = mg_bind_opt(&mgr, s_http_port, ev_handler, bind_opts);
   if (nc == NULL) {
     fprintf(stderr, "Error starting server on port %s: %s\n", s_http_port,
