0bae1d88cce38b6f805f90162242fbadeac91a49
aswasn <aswasn@qq.com>
2018-03-23 23:23:54 +0800

    some ajax

--- www/js/common.js
+++ www/js/common.js
@@ -16,6 +16,17 @@ function check_login() {
     }
 };
 
+(function($) {
+    $.getUrlParam = function(name) {
+        var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+        var r = window.location.search.substr(1).match(reg);
+        if (r != null) {
+            return unescape(r[2]);
+        }
+        return null;
+    };
+})(jQuery);
+
 $(function(){
     check_login();
 });
--- www/js/detail.js
+++ www/js/detail.js
@@ -1,5 +1,66 @@
+function get_diary_content() {
+    const d_id = $.getUrlParam("diary_id");
+    $.ajax({
+        url: "api/get_diary_content",
+        method: "post",
+        data: {diary_id: d_id},
+        dataType: "json"
+    }).done(function(msg) {
+        $("#content,#edit-textarea").text(msg.content);
+    });
+}
 
+function get_comments() {
+    const d_id = $.getUrlParam("diary_id");
+    $.ajax({
+        url: "api/get_comments",
+        method: "post",
+        data: {diary_id: d_id},
+        dataType: "json"
+    }).done(function(msg) {
+        console.log(msg);
+    });
+}
 
-$(function(){
+function send_comment() {
+    const d_id = $.getUrlParam("diary_id");
+    const text = $("#comment-input").val();
+    const u_id = Cookies.get("user");
+    $.ajax({
+        url: "api/add_comment",
+        method: "post",
+        data: {
+            diary_id: d_id,
+            content: text,
+            user_id: u_id
+        },
+        dataType: "json"
+    }).done(function(msg) {
+        console.log(msg);
+    });
+}
 
+function submit_edit() {
+    const d_id = $.getUrlParam("diary_id");
+    const text = $("#edit-textarea").val();
+    const u_id = Cookies.get("user");
+    $.ajax({
+        url: "api/edit_diary",
+        method: "post",
+        data: {
+            diary_id: d_id,
+            content: text,
+            user_id: u_id
+        },
+        dataType: "json"
+    }).done(function(msg) {
+        console.log(msg);
+    });
+}
+
+$(function() {
+    get_diary_content();
+    get_comments();
+    $("#send-comment-btn").on("click", send_comment);
+    $("#submit-edit-btn").on("click", submit_edit);
 });
