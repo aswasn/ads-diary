ca5f703675d1a3ae9710b1718918b06449e53d02
aswasn <aswasn@qq.com>
2018-03-24 19:29:49 +0800

    fix like

--- mongoose/server.cpp
+++ mongoose/server.cpp
@@ -322,7 +322,7 @@ static void handle_like(struct mg_connection *nc, struct http_message *hm) {
     reply = REDIS_COMMAND(redis_cli, "INCR %s", redis_d_id);
     /* Send response */
     mg_printf(nc, "%s", "HTTP/1.1 200 OK\r\nTransfer-Encoding: chunked\r\n\r\n");
-    mg_printf_http_chunk(nc, "ok");
+    mg_printf_http_chunk(nc, "%s", "{ \"success\": 1 }");
     mg_send_http_chunk(nc, "", 0); /* Send empty chunk, the end of response */
 }
 
