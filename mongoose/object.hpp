ad87ea97f976712bd4e2d2d8de8254d6132f098c
StrikeW <wangsiyuanse@gmail.com>
2018-03-24 23:53:09 +0800

    fix diary

--- mongoose/object.hpp
+++ mongoose/object.hpp
@@ -26,11 +26,12 @@ namespace objects {
     };
 
     void to_json(json& j, const diary& d) {
-        j = json{{"id", d.id}, {"user", d.user}, {"content", d.content}, {"utime", d.utime}};
+        j = json{{"id", d.id}, {"ver", d.ver}, {"user", d.user}, {"content", d.content}, {"utime", d.utime}};
     }
 
     void from_json(const json& j, diary& d) {
         d.id = j.at("id").get<int>();
+        d.ver = j.at("ver").get<int>();
         d.user = j.at("user").get<std::string>();
         d.content = j.at("content").get<std::string>();
         d.utime = j.at("utime").get<uint64_t>();
