2e1736ee1184be39c66c57f9d0da8c6c55e6171a
StrikeW <wangsiyuanse@gmail.com>
2018-03-24 19:15:29 +0800

    fix get_like

--- mongoose/server.cpp
+++ mongoose/server.cpp
@@ -303,7 +303,7 @@ static void handle_get_like(struct mg_connection *nc, struct http_message *hm) {
 
     /* Send headers */
     mg_printf(nc, "%s", "HTTP/1.1 200 OK\r\nTransfer-Encoding: chunked\r\n\r\n");
-    like.num = atoi(reply->str);
+    like.num = reply->str == NULL ? 0 : atoi(reply->str);
     json j = like;
     mg_printf_http_chunk(nc, "%s", j.dump().c_str());
     mg_send_http_chunk(nc, "", 0); /* Send empty chunk, the end of response */
